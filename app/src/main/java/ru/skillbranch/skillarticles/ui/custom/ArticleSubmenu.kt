ddb2aa53848135f9138602093f40a293950f26e2
aleksandr.voloshin <aleksandr.voloshin@service-it.by>
2021-06-12 16:20:49 +0300

    submenu deleted

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/ArticleSubmenu.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/ArticleSubmenu.kt
@@ -7,27 +7,21 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.ViewAnimationUtils
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.animation.doOnEnd
 import androidx.core.animation.doOnStart
 import com.google.android.material.shape.MaterialShapeDrawable
 import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.extensions.dpToPx
-import ru.skillbranch.skillarticles.ui.custom.behaviors.SubMenuBehavior
 import kotlin.math.hypot
 
 class ArticleSubmenu @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : ConstraintLayout(context, attrs, defStyleAttr), CoordinatorLayout.AttachedBehavior {
+) : ConstraintLayout(context, attrs, defStyleAttr) {
     var isOpen = false
     private var centerX: Float = context.dpToPx(200)
     private var centerY: Float = context.dpToPx(96)
-
-    override fun getBehavior(): CoordinatorLayout.Behavior<*> {
-        return SubMenuBehavior()
-    }
 
     init {
         View.inflate(context, R.layout.layout_submenu, this)
